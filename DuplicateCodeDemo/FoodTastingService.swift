4593ff668489c956c0113b03d007b1ff970df680
sanimudah <sani@mudah.my>
2025-04-23 10:33:30 +0800

    Add food tasting service.

---
+++ DuplicateCodeDemo/FoodTastingService.swift
@@ -0,0 +1,12 @@
+//
+//  FoodTastingService.swift
+//  DuplicateCodeDemo
+//
+//  Created by Abd Sani Abd Jalal on 23/04/2025.
+//
+
+class FoodTastingService {
+    func checkIfFoodIsGood(title: String) -> Bool {
+        title == "pizza"
+    }
+}
