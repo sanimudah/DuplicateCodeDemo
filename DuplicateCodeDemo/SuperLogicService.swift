b9a5b570c7becf329d3cd4603367882638a5ceaf
sanimudah <sani@mudah.my>
2025-04-23 10:31:45 +0800

    Add Super Logic Service

---
+++ DuplicateCodeDemo/SuperLogicService.swift
@@ -0,0 +1,12 @@
+//
+//  SuperLogicService.swift
+//  DuplicateCodeDemo
+//
+//  Created by Abd Sani Abd Jalal on 23/04/2025.
+//
+
+class SuperLogicService {
+    func checkIfFoodIsGood(title: String) -> Bool {
+        title == "pizza"
+    }
+}
