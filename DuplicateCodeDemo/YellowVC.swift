abb5e4a49147e0abaf09525016a2a4f50a47bfad
sanimudah <sani@mudah.my>
2025-04-23 10:04:40 +0800

    Add yellow VC.

---
+++ DuplicateCodeDemo/YellowVC.swift
@@ -0,0 +1,30 @@
+//
+//  YellowVC.swift
+//  DuplicateCodeDemo
+//
+//  Created by Abd Sani Abd Jalal on 23/04/2025.
+//
+
+import UIKit
+
+class YellowVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .yellow
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
